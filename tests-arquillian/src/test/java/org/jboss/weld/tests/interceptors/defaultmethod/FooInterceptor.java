f6ec771bc2495fe4222879f3cc5a07e236e214c3
Matej Novotny <manovotn@redhat.com>
2017-07-19 13:17:40 +0200

    WELD-2405 attempt to create a test using given version of classfilewriter.

--- tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/defaultmethod/FooInterceptor.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/defaultmethod/FooInterceptor.java
@@ -1,11 +1,13 @@
 package org.jboss.weld.tests.interceptors.defaultmethod;
 
+import javax.annotation.Priority;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
 
 @FooInterceptorBinding
 @Interceptor
+@Priority(1000)
 public class FooInterceptor {
 
     @AroundInvoke
--- tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/defaultmethod/InterceptedBeanWithDefaultMethodTest.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/defaultmethod/InterceptedBeanWithDefaultMethodTest.java
@@ -1,26 +1,39 @@
 package org.jboss.weld.tests.interceptors.defaultmethod;
 
+import java.io.File;
+
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.BeanArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.jboss.weld.test.util.Utils;
+import org.jboss.weld.tests.category.EmbeddedContainer;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
+// use embedded to avoid versions from WFLY
+@Category(EmbeddedContainer.class)
 public class InterceptedBeanWithDefaultMethodTest {
 
    @Inject
    private Foo foo;
 
    @Deployment
-   public static Archive<?> deploy() {
-      return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(InterceptedBeanWithDefaultMethodTest.class)).intercept(FooInterceptor.class).addPackage(InterceptedBeanWithDefaultMethodTest.class.getPackage());
+   public static WebArchive deploy() {
+       
+       // TODO - does not work, still uses new one and passes
+      File oldClassFileWriter = Maven.resolver().resolve("org.jboss.classfilewriter:jboss-classfilewriter:1.1.2.Final").withTransitivity().asSingleFile();
+      return ShrinkWrap.create(WebArchive.class, Utils.getDeploymentNameAsHash(InterceptedBeanWithDefaultMethodTest.class, Utils.ARCHIVE_TYPE.WAR))
+          .addAsLibraries(oldClassFileWriter) // force old classfilewriter on CP
+          .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+          .addPackage(InterceptedBeanWithDefaultMethodTest.class.getPackage());
    }
 
    /*
